e2e7c095d7a3351dec60a010a444b9d1975bdd4c
Gaurav Agarwal <gauravagarwalr@gmail.com>
2018-06-17 21:18:58 +0530

    Adding lossless code (~ 1/3rd size in webp). Moving to Dir.

--- optimize-png.rb
+++ optimize-png.rb
@@ -2,14 +2,24 @@
 
 require 'json'
 
-dir_name = ARGV.first
+pic_dir = ARGV.first
 
-all_files = Dir["./#{dir_name}/*.png"]
+all_files = Dir["./#{pic_dir}/*.png"]
 
 all_files.each do |file|
   webp_name = file.sub(".png", ".webp")
 
   puts file + " => " + webp_name
 
+  # Lossy
   `cwebp "#{file}" -o "#{webp_name}"`
+
+  # Lossless
+  # `cwebp -lossless "#{file}" -o "#{webp_name}"`
 end
+
+`mkdir -p "#{pic_dir}/Compressed"`
+command = "mv '#{pic_dir}/'*.webp '#{pic_dir}/Compressed/'"
+
+puts command
+exec command
--- optimize.sh
+++ optimize.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
+set -e
+
 for f in $1/**/*
 do
+  echo $f
   ./optimize-png.rb "$f"
 done
